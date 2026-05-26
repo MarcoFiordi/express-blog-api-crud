efe79e0fe77a833c77f8a1dbe6fdf7948c859783
Marco Fiordi <marco.fiordi97@gmail.com>
2026-05-26 17:06:19 +0200

    implementata destroy dei post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,22 +1,22 @@
 import posts from "../data/posts.js";
 
 
-function index(request, response){
+function index(request, response) {
     response.status(200).json(posts);
 }
 
 
-function show(request, response){
+function show(request, response) {
     const id = request.params.id;
     const realId = Number(id.trim());
-    if (isNaN(realId)){
+    if (isNaN(realId)) {
         response.status(400)
             .json({
                 error: 'parametro id non corretto'
             });
         return;
     }
-    if (realId <= 0){
+    if (realId <= 0) {
         response.status(400)
             .json({
                 error: 'parametro id negativo o zero'
@@ -29,7 +29,7 @@ function show(request, response){
         return post.id === realId
     });
 
-    if (postFound === undefined){
+    if (postFound === undefined) {
         response.status(404)
             .json({
                 error: 'post non trovato',
@@ -50,24 +50,24 @@ function store(request, response) {
     })
 }
 
-function update(request, response){
+function update(request, response) {
     const id = request.params.id;
     response.json({
-        messaggio:`modifica del post ${id}`
+        messaggio: `modifica del post ${id}`
     })
 }
-function modify(request, response){
+function modify(request, response) {
     const id = request.params.id;
     response.json({
         messaggio: `modifica parziale del post ${id}`
     })
 
 }
-function destroy(request, response){
+function destroy(request, response) {
     const id = request.params.id;
     const realId = Number(id.trim());
 
-    if (isNaN(realId)){
+    if (isNaN(realId)) {
         response.status(400).json({
             error: 'parametro id non corretto'
         });
@@ -80,15 +80,36 @@ function destroy(request, response){
         });
         return;
     }
-    response.json({
-        messaggio:`distruzione del post ${id}`
-    })
+    const postIndex = posts.findIndex(post => {
+        return post.id === realId;
+    });
+
+    if (postIndex === -1) {
+        response.status(404).json({
+            error: 'post non trovato',
+            result: null
+        });
+        return;
+    }
+
+    posts.splice(postIndex, 1);
+
+    console.log(posts);
+
+
+
+
+    response.status(200).json({
+        messaggio: `distruzione del post ${realId}`
+    });
 }
 
 
-export { index,
+export {
+    index,
     store,
     update,
     modify,
     destroy,
-    show};
+    show
+};
